a2cc3ef71e3d45e8939973fcb1e7ec493ef456f5
Rob Woodgate <robwoodgate@mac.com>
2025-02-27 19:45:50 +0000

    Fix autopay

--- src/main.js
+++ src/main.js
@@ -132,7 +132,7 @@ $(function($) {
       // Autopay?
       let params = new URL(document.location.href).searchParams;
       let autopay = decodeURIComponent(params.get('autopay') ?? '');
-      if (autopay) {
+      if (autopay && $lnurl.val().length) {
         await makePayment();
       }
     } catch (err) {
